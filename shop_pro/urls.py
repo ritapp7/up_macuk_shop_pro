11a4099cd63b844b20718816ba180f333975577f
Bubleks <gugugaga23166@gmail.com>
2025-10-16 09:49:41 +0500

    шаблон

--- shop_pro/urls.py
+++ shop_pro/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from shop_app.views import dfg
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', dfg)
 ]
